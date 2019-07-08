c0e51e4f2d7800905190234f5e64bc73ae20a7cf
jody-davids-07 <Your Actual Email>
2019-07-08 09:01:24 +0100

    Moved pop-up text from html file to js file

--- javascript/weather-conditions.js
+++ javascript/weather-conditions.js
@@ -14,34 +14,62 @@ function displayDressCode(){
         x.src = "images/warmgirl.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
 
+        <div class="popup" onclick="displayDressCode()">
+         <span class="popuptext" id="myPopup">Due to the weather condition stated, it is suggested you wear something tightly buttoned such as a raincoat, wellingtons and don't forget your umbrella!</span>
+        </div>
+
     }else if (temperature >= 0 && temperature <= 10) {
         var x = document.createElement('img'); //creating an image
         x.src = "images/raining.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
+
+        <div class="popup" onclick="displayDressCode()">
+         <span class="popuptext" id="myPopup">Due to the weather condition stated, it is suggested you wear something warm such as a coat, beenie, scarf and closed shoes.</span>
+        </div>  
 
     }else if (temperature >= 11 && temperature <= 15 ) {
         var x = document.createElement('img'); //creating an image
         x.src = "images/winterboy.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
 
+        <div class="popup" onclick="displayDressCode()">
+         <span class="popuptext" id="myPopup">Due to the weather condition stated, it is suggested you wear something warm such as a trackpants, sneakers, long-sleeve everthing.</span>
+        </div>  
+
     }else if (temperature >= 16 && temperature <= 20 ) {
         var x = document.createElement('img'); //creating an image
         x.src = "images/winterboy.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
+
+        <div class="popup" onclick="displayDressCode()">
+         <span class="popuptext" id="myPopup">Due to the weather condition stated, it is suggested you wear a windbreaker and a keep your ears and neck warm with a beenie and scarf.</span>
+        </div>  
 
     }else if (temperature >= 21 && temperature <= 24 )  {
         var x = document.createElement('img'); //creating an image
         x.src = "images/winterboy.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
 
+        <div class="popup" onclick="displayDressCode()">
+         <span class="popuptext" id="myPopup">Due to the weather condition stated, it is suggested you wear something to keep you warm, but not too humid so e.g a scarf or warm jacket.</span>
+        </div>  
+
     }else if (temperature >= 25 && temperature <=45) {
         var x = document.createElement('img'); //creating an image
         x.src = "images/hot.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
 
+        <div class="popup" onclick="displayDressCode()">
+         <span class="popuptext" id="myPopup">Due to the weather condition stated, it is suggested you wear something cool such as a hat, short and flip-flops also don't forget to wear sunscreen!.</span>
+        </div>
+
     } else{
         var x = document.createElement('img'); //creating an image
         x.src = "images/winterboy.gif"; //calling relative pathway of image
-        document.body.appendChild(x); //append the image to the body     
+        document.body.appendChild(x); //append the image to the body  
+        
+        <div class="popup" onclick="displayDressCode()">
+         <span class="popuptext" id="myPopup">Sorry, invalid temperature. Try again.</span>
+        </div>
     }
 }
