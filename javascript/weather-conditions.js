25fab1ed59acf6706cc0653e794317690354e0bc
jody-davids-07 <Your Actual Email>
2019-07-08 11:29:27 +0100

    Added new images and new text to the popups

--- javascript/weather-conditions.js
+++ javascript/weather-conditions.js
@@ -25,29 +25,29 @@ function displayDressCode(){
         
     }else if (temperature >= 11 && temperature <= 15 ) {
         var x = document.createElement('img'); //creating an image
-        x.src = "images/winterboy.gif"; //calling relative pathway of image
+        x.src = "images/windy.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
 
         function alertpopup(){
-            alertpopup("Due to the weather condition stated, it is suggested you wear something tightly buttoned such as a raincoat, wellingtons and don't forget your umbrella!"); 
+            alertpopup("Due to the weather condition stated, it is suggested you wear a wind-breaker, sneakers and long pants"); 
            }
 
     }else if (temperature >= 16 && temperature <= 20 ) {
         var x = document.createElement('img'); //creating an image
-        x.src = "images/winterboy.gif"; //calling relative pathway of image
+        x.src = "images/.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
 
         function alertpopup(){
-            alertpopup("Due to the weather condition stated, it is suggested you wear something tightly buttoned such as a raincoat, wellingtons and don't forget your umbrella!"); 
+            alertpopup("Due to the weather condition stated, it is suggested you wear something airy, but keep a jersey by your side just incase."); 
            } 
 
     }else if (temperature >= 21 && temperature <= 24 )  {
         var x = document.createElement('img'); //creating an image
-        x.src = "images/winterboy.gif"; //calling relative pathway of image
+        x.src = "images/cool.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
 
         function alertpopup(){
-            alertpopup("Due to the weather condition stated, it is suggested you wear something tightly buttoned such as a raincoat, wellingtons and don't forget your umbrella!"); 
+            alertpopup("Due to the weather condition stated, it is suggested you wear something light, go out and enjoy the sunshine."); 
            } 
 
     }else if (temperature >= 25 && temperature <=45) {
@@ -56,16 +56,16 @@ function displayDressCode(){
         document.body.appendChild(x); //append the image to the body
 
         function alertpopup(){
-            alertpopup("Due to the weather condition stated, it is suggested you wear something tightly buttoned such as a raincoat, wellingtons and don't forget your umbrella!"); 
+            alertpopup("Due to the weather condition stated, it is suggested you wear something beach-eeey, shorts and a vest, oh and don't forget the sunscreen!"); 
            }
 
     } else{
         var x = document.createElement('img'); //creating an image
-        x.src = "images/winterboy.gif"; //calling relative pathway of image
+        x.src = "images/oops.gif"; //calling relative pathway of image
         document.body.appendChild(x); //append the image to the body
         
         function alertpopup(){
-            alertpopup("Due to the weather condition stated, it is suggested you wear something tightly buttoned such as a raincoat, wellingtons and don't forget your umbrella!"); 
+            alertpopup("Sorry, invalid temperature. Try again."); 
            }
         
     }
